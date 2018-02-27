08fd1f73d0b992308c43738496fe56665368f305
PolarisTLX <paul.a.rail@gmail.com>
2018-02-27 07:58:38 -0500

    uploading intro

---
+++ Microverse Collaboration Project First Intro.js
@@ -0,0 +1,60 @@
+/*
+-We probably dont need to rush or feel stressed.
+
+-Read our setup / getting ready sections.
+We are instructed to do this part alone and with mics off.
+
+-Check that screensharing and mics are working properly/are loud enough.
+
+-Let me know if I am speaking to quickly or speaking too much?
+(sorry for my coughing)
+
+-I think we need to record the whole 2 days and upload to Youtube,
+need to make sure this is working properly.
+collecting the audio from both? Collecting both screens?
+
+-Perhaps discuss what areas you feel very comfortable in (with respect to this project)
+-What areas you feel less comfortable in.
+
+-GitHub / code sharing  (open to suggestion here if you know of a better/cleaner/faster way to work on the same code).
+
+-Perhaps one just start a general project quickly, just so the other can observe their approach/method?
+-Discuss preferences for approach, IDE/editor, shortcuts etc.
+
+
+-Discuss requirements and line them up to be sure they are all addressed by the end.
+-Discuss resources and get them prepared (browser tabs open to those pages).
+
+-Start a new project on GitHub
+(will this be in just one of our accounts or does it have to be a shared account?)
+
+
+-Take turns, the one coding should talk out lout about what they are doing.
+-pausing for questions and comments, find rhythm.
+-pausing if one spots a mistake or typo that breaks code.
+-We might need to go slower so that the other can follow and comment properly?
+
+
+-Maybe add extra features once the main part is done.
+What things does any major website have?
+  -Navbar
+  -logo
+  -footer
+  -contact me section? (this is more complex than it sounds)
+  -a couple interactive items to demonstrate a variety of types of function,
+
+-Add comments throughout.
+-Check for typos.
+
+
+-Set general predicted timelines to keep us on track:
+If we have 8am-5pm for two days. Then perhaps:
+Day 1 - Noon: We should be putting some real code down by this point, at least foundation work / boilerplate code.
+Day 1 - 2pm: We should be starting to check out the major requirements.
+Day 1 - 4pm: Start reviewing, is our progress on track for the requirements?
+             What was causing us problems?
+             Anything we can do better / improve upon for Day 2 to make things smoother?
+Day 2 - 8am: We should be addressing the major big requirements.
+Day 2 - Noon: Start thinking about Final comments and polish.
+
+*/
